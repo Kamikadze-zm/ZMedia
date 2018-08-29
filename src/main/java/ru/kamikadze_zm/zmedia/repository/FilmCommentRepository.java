9f4f410099a6737f90ced4af40edf97deb33942f
Pavel Ryabov <Kamikadze.zm@gmail.com>
2018-08-29 16:06:34 +1000

    Добавлены репозитории фильмов и комментариев к фильмам

---
+++ src/main/java/ru/kamikadze_zm/zmedia/repository/FilmCommentRepository.java
@@ -0,0 +1,7 @@
+package ru.kamikadze_zm.zmedia.repository;
+
+import ru.kamikadze_zm.zmedia.model.entity.FilmComment;
+
+public interface FilmCommentRepository extends CommentRepository<FilmComment> {
+    
+}
---
+++ src/main/java/ru/kamikadze_zm/zmedia/repository/FilmRepository.java
@@ -0,0 +1,7 @@
+package ru.kamikadze_zm.zmedia.repository;
+
+import ru.kamikadze_zm.zmedia.model.entity.Film;
+
+public interface FilmRepository extends PublicationRepository<Film> {
+    
+}
