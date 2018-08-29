7e35631fea1a3424e1c64bacd1302216d13d28e9
Pavel Ryabov <Kamikadze.zm@gmail.com>
2018-08-29 20:14:39 +1000

    Добавлены репозитории сериалов и комментариев к сериалам

---
+++ src/main/java/ru/kamikadze_zm/zmedia/repository/TvSeriesCommentRepository.java
@@ -0,0 +1,7 @@
+package ru.kamikadze_zm.zmedia.repository;
+
+import ru.kamikadze_zm.zmedia.model.entity.TvSeriesComment;
+
+public interface TvSeriesCommentRepository extends CommentRepository<TvSeriesComment> {
+    
+}
---
+++ src/main/java/ru/kamikadze_zm/zmedia/repository/TvSeriesRepository.java
@@ -0,0 +1,7 @@
+package ru.kamikadze_zm.zmedia.repository;
+
+import ru.kamikadze_zm.zmedia.model.entity.TvSeries;
+
+public interface TvSeriesRepository extends PublicationRepository<TvSeries> {
+
+}
